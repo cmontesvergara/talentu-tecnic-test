91f6ed618caf658f0d35e25ca5d9f388a0b922b6
Carlos Montes <krlosbergara@gmail.com>
2022-05-18 20:52:39 -0500

    deploy

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { inject, onMounted, watch, ref } from "vue";
+import { inject, onActivated, watch, ref } from "vue";
 import { store } from "../store/store.js";
 const { getLocalUsers } = inject("homeFuntions");
 
@@ -40,7 +40,7 @@ function getAge(date) {
   return age;
 }
 
-onMounted(() => {
+onActivated(() => {
   updateUserList();
 });
 </script>
